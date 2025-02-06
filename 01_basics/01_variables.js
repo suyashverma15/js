bb1c62abc13cddcf8df4ac0b060bc82967256365
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-06 19:05:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234
+let accountEmail = "abc@gmail.com"
+var accountPassward = "98765"
+accountCity = "Ayodhya"
+let accountstate ;
+
+
+// accountId = 4321 // not allowed
+
+/*
+generally we do not use var variable because it has functional issue and scope issue.
+*/
+
+accountEmail = "man@gmail.com"
+accountPassward = "5678"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountstate]);
+
+
