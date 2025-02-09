a7121b52bd282f62a23e65bae50e7de42635181d
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-09 19:11:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "1234acb"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let score = "null"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let someNumber = 33
+let stirngNumber = String(someNumber)
+console.log(stirngNumber);
+console.log(typeof stirngNumber);
