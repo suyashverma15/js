dd0db23245f3ed97d0aca31b8f24baa76c3a26cd
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-09 18:47:21 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,26 @@
-"use strict" // treat all js code as newer version
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs not browswer
+
+let name = "Suyash"
+let age  = 20
+let isLoggedIn = false
+
+// data types - 
+// number
+// bigInt
+// stirng => ""
+// boolean => truee/false
+// null => standlone value
+// undefined => value is not assigned yet
+// symbol => we use this for the use of Uniqueness.
+
+// object
+
+console.log(typeof "Suyash");
+
+console.log(typeof 18);
+
+console.log(typeof null);  // output is object
+
+console.log(typeof undefined); // output is undefined
