6bc3b5226374553bb06f446afcbc63bd8e7b506c
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-09 19:36:22 +0000

    datatypes and conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,32 @@ let valueInNumber = Number(score)
 
 let someNumber = 33
 let stirngNumber = String(someNumber)
-console.log(stirngNumber);
-console.log(typeof stirngNumber);
+// console.log(stirngNumber);
+// console.log(typeof stirngNumber);
+
+
+// ******************************Operation*******************************
+
+let value = 8
+let negVlaue = -value
+// console.log(negVlaue);
+
+let str1 = "hello"
+let str2 = " Suyash"
+let str3 = str1 + str2;
+// console.log(str3)
+
+
+// console.log("1" + 2) => 12
+// console.log(1 + "2") => 12
+// console.log("1" + 2 + 2) => 122
+// console.log(1 + 2 + "2") => 32
+  
+
+// console.log(true) => true
+// console.log(+true) => 1
+// console.log(true+) => error 
+// console.log(+"")  => 0
+
+// read prefix and postfix from mdn
+
