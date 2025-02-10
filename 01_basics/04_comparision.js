4fd10988bdaee8d0c34d7010e677b6d752778ef0
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-10 18:25:13 +0000

    datatypes comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// strict check (===) ye na sirf value ko check krta h but also check the data type strictly
+
+
