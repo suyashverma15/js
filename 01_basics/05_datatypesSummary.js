20def322774bbbbc60eb2cd32f908209126efadd
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-12 18:45:27 +0000

    Datatype summary

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -15,7 +15,7 @@ let userEmail;
 
 const Id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(Id === anotherId);
+// console.log(Id === anotherId);
 
 // # Reference type (Non primitive): in this values ka reference memory me direct allocate kiya ja skta h)
 //                               eg - Array,  Object, Function.
@@ -27,6 +27,37 @@ let myObj = {
 }
 
 let myFunction = function(){
-    console.log("Hello World");
+    // console.log("Hello World");
     
 }
+
+
+// **********************************************************************************
+
+// Stack(primitive)       Heap(Non-primitive)
+
+let myCtiy = "Ayodhya"
+let anotherCity = myCtiy
+anotherCity = "Lucknow"
+
+console.log(myCtiy);
+console.log(anotherCity);
+
+let userOne = {
+    email: "one@Paytm.com",
+    transaction: "user@ybl"
+}
+let userTwo = userOne;
+userTwo.email = "two@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
+
+
+
+
