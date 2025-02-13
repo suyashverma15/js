b9e750899a78b91753ded22e75b539eb518146f7
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-13 18:49:17 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,20 @@
+const name = "Suyash"
+const repoCount = 30
+//  console.log(name + repoCount + " Vlaues"); // this is old version. 
+ 
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);   // this is new and best methio to  print 
+
+
+const gameName = new String("Suyash")
+// console.log(gameName[0]); // letter position in string
+// console.log(gameName.__proto__);  
+
+console.log(gameName.length); 
+console.log(gameName.toUpperCase()); // convert string in uppercase
+
+console.log(gameName.charAt(3)); // this will give the char at 3rd index in the string
+
+console.log(gameName.indexOf('y')); // this will give the imdex of char y  in the string
+
+
+
