e68264565f2394dbed9c8787cf4b9239217002d9
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-23 12:52:41 +0000

    dates

---
+++ 01_basics/08_dates_in_js.js
@@ -0,0 +1,32 @@
+// dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toLocaleTimeString);
+
+// console.log(typeof myDate); // date ka type object hai
+
+// let myCreatedDate = new Date(2025,0,23);
+//let myCreatedDate = new Date(2025,0,23, 5 ,3);
+let myCreatedDate = new Date("23-1-2025")
+
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+
