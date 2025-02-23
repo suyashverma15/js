6c77edd68ade4ab43f19197ab15cc3bb4dfe65d3
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-23 06:31:10 +0000

    number&maths

---
+++ 01_basics/07_num_maths.js
@@ -0,0 +1,43 @@
+const number = 400
+
+// console.log(number);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2)); // it will give the value of last 2 digit after decimal.
+
+const otherNoumber = 12.89456;
+ // console.log(otherNoumber.toPrecision(3));
+
+const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString('en-IN')); // it will seperste with , in between zeros.
+
+
+// **********************************maths****************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.67));
+// console.log(Math.round(-5.67));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(3, 7 ,0 ,1 , 3 ,2, 9));
+// console.log(Math.max(3, 7 ,0 ,1 , 3 ,2, 9));
+
+
+console.log(Math.random()); // math.random give the value in the range of zero to one
+
+
+
+
+
+ 
+
+
+ 
+
+
