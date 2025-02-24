a0106ac5a76bf494c599e24a914be259dc7a0738
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-24 08:20:17 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+//Array
+
+const myArray = [1,2,4,5,6,7,9,8];
+const myHeros = ["vk", "msd", "rs"];
+
+const myArray2 = new Array(1,2,3,4,5,6); // another declaration method
+
+// console.log(myArray[2]);
+
+// Arrays method
+
+// myArray.push(6);
+// myArray.push(10);
+// myArray.pop(); // remove the last value of the array
+
+// myArray.unshift(22); // add the value in the begining of array by which all the index of value will be chnage
+// myArray.shift() // remove the value form the starting of index of array
+
+// console.log(myArray.includes(10));
+// console.log(myArray.indexOf(5));
+
+
+
+
+
+// console.log(myArray);
