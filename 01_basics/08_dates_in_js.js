7eb615d7d210b34e54c2d9fbf67f596f14a5dc63
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-24 07:11:13 +0000

    date and time

--- 01_basics/08_dates_in_js.js
+++ 01_basics/08_dates_in_js.js
@@ -19,9 +19,21 @@ let myCreatedDate = new Date("23-1-2025")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now();
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000));
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
+
+
+
 
 
 
