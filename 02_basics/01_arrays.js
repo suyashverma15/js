a01f0415451d2a21cb1de75818fd83d245ed2788
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-02-27 19:24:01 +0000

    array1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -16,7 +16,7 @@ const myArray2 = new Array(1,2,3,4,5,6); // another declaration method
 // myArray.unshift(22); // add the value in the begining of array by which all the index of value will be chnage
 // myArray.shift() // remove the value form the starting of index of array
 
-// console.log(myArray.includes(10));
+// console.log(myArray.includes(10)); // value is present or not
 // console.log(myArray.indexOf(5));
 
 
@@ -24,3 +24,23 @@ const myArray2 = new Array(1,2,3,4,5,6); // another declaration method
 
 
 // console.log(myArray);
+
+
+// const newArr = myArray.join(); // adds all the element of an array into string
+// console.log(myArray);
+// console.log(typeof newArr);
+
+// slice and splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3); // range from 1 to 3 but not include 3
+console.log(myn1);
+console.log("B ", myArray);
+ 
+
+const myn2 = myArray.splice(1, 3);
+console.log(myn2);
+
+
+
