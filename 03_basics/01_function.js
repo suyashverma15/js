ab8414cb9061ba5dfedde7c6cc3e5ba5254672bf
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-03-05 19:33:29 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,19 @@
+// function addTwoNumbers(number1, number2){
+//     console.log(number1+number2);   
+// }
+
+ // addTwoNumbers(3,4)
+
+ function addTwoNumbers(number1, number2){
+   const res = number1 + number2
+   return res;
+
+}
+
+const res = addTwoNumbers(3,4)
+// console.log("Result :",  res);
+
+function loginUsermessage(username){
+    return  `${username} just loggedin`
+}
+console.log(loginUsermessage("Suyash"));
