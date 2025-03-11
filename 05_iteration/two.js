43d3bbc501bf21084204c72eac12c06a4058fe72
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-03-11 15:42:50 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,24 @@
+// while loop
+
+
+// let i = 0
+// while (i <= 3) {
+//     console.log(`value of i is ${i}`);
+//     i++;
+// }
+
+// let myArray = ["cvbn", "oiuhg","fyui"]
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+//*************do while loop************** */
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score++;  
+} while (score <= 10);
