353b7672e5fb17911c5e3efc0f0719ea45799b10
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-03-12 15:37:44 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,36 @@
+// to Iterate the Object we use for in loop
+
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    py: "Python"
+}
+// for (const key in myObject) {
+//     console.log(key);
+    
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} used for ${myObject[key]}`);
+    
+// }
+
+
+// use of for in loop  in Array
+
+// const programming = ["cpp","py","js","rb"];
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('usa', "United State Of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
