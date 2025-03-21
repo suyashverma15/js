15786a90e97559966aaf525935449c46e856df49
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-03-21 09:44:41 +0000

    filter

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -21,16 +21,40 @@
 
 // print upper array using foreach loop
 
-const nums = [1,2,3,4,5,6,7,8,9,10];
+// const nums = [1,2,3,4,5,6,7,8,9,10];
 
-const newNums = []
-nums.forEach( (it) => {
-    if(it > 4){
-        newNums.push(it)
+// const newNums = []
+// nums.forEach( (it) => {
+//     if(it > 4){
+//         newNums.push(it)
         
-    }
+//     }
+// })
+// console.log(newNums);
+
+
+const books = [
+    {title: 'book one', gener: 'fiction', publish: 1981, edition: 2004},
+    {title: 'book two', gener: 'non-fiction', publish: 1992, edition: 2008},
+    {title: 'book three', gener: 'history', publish: 1999, edition: 2007},
+    {title: 'book four', gener: 'non-fiction', publish: 1989, edition: 2010 },
+    {title: 'book five', gener: 'science', publish: 2009, edition: 2014},
+    {title: 'book six', gener: 'fiction', publish: 1987, edition: 2010},
+    {title: 'book seven', gener: 'history', publish: 1986, edition: 1996},
+    {title: 'book eight', gener: 'science', publish: 2011, edition: 2016}
+
+];
+
+// let userBooks = books.filter( (bk) => bk.gener === 'science');
+
+// userBooks = books.filter( (it) => {return it.edition >= 2002});
+
+const userBook = books.filter( (it) => {
+    return it.gener === "fiction" && it.edition >= 2000
 })
-console.log(newNums);
+console.log(userBook);
+
+
 
 
 
